05d4bb0adea5d95fe5742454d9de6fb03062f217
aracdo <40762424+aracdo@users.noreply.github.com>
2020-10-06 15:23:17 +0300

    Create main.sql

---
+++ task1/main.sql
@@ -0,0 +1,29 @@
+CREATE TABLE IF NOT EXISTS users (
+	user_id serial CONSTRAINT USER_ID_PK PRIMARY KEY,
+	user_name varchar(60) NOT NULL); 
+CREATE TABLE IF NOT EXISTS liks (
+	from_user serial,
+	to_user serial,
+	FOREIGN KEY (from_user)
+	REFERENCES users(user_id),
+	FOREIGN KEY (to_user)
+	REFERENCES users(user_id),
+    CONSTRAINT liks_user_from_user_to PRIMARY KEY (from_user, to_user),
+    CONSTRAINT liks_check CHECK (from_user <> to_user)
+);
+
+INSERT INTO users (user_id, user_name)
+     VALUES (3, 'Сидоров Сидор Сидорович'),
+            (1, 'Иванов Иван Иванович'),
+            (2, 'Петров Пётр Петрович');
+
+INSERT INTO liks (from_user, to_user)
+     VALUES (1, 3),
+            (2, 3),
+            (2, 3),
+            (2, 3),
+            (1, 2);
+select count(*) from liks group by (from_user);
+SELECT users.user_name as user_name, 
+(select count(*) from liks WHERE liks.from_user = users.user_id) AS liked_from,
+(select count(*) from liks WHERE liks.to_user = users.user_id) AS liked_to FROM users ORDER BY user_name;
